c7d811e196313d3d6e44e5d2688cc5df76b1e049
magda <magdalena.aniol@gmail.com>
2018-07-25 13:43:55 +0200

    add todo component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import style from './TodoList.css';
+import Todo from './Todo.js'
 
 const TodoList = (props) => {
 	const listItems = props.data.map((item) =>
-		<li key={item.id} href="#" onClick={() => props.removeTodo(item.id)}>{item.text}</li>
+		<li key={item.id} href="#" onClick={() => props.removeTodo(item.id)}><Todo value={item.text}/></li>
 	);
 	return <div className={style.TodoList}><ul>{listItems}</ul></div>
 }
